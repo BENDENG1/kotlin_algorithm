3c4c242d0c28025caacf6528b9ce57f5a8862389
BENDENG1 <76191161+BENDENG1@users.noreply.github.com>
2022-10-27 16:34:29 +0900

    Update 10_27_2750.kt

--- Basic/10_27_2750.kt
+++ Basic/10_27_2750.kt
@@ -1,7 +1,19 @@
 package com.gyroh.algorithm
 
+import java.util.*
+
 fun main(){
+    var input = Scanner(System.`in`)
     var n : Int = 0
+    n = input.nextInt()
+    var arr = Array<Int>(n,{0})
 
-
+    for (i in 0 until n){
+        var num =  input.nextInt()
+        arr.set(i,  num)
+    }
+    arr.sort()
+    for(i in 0 until n){
+        println("${arr[i]}")
+    }
 }
